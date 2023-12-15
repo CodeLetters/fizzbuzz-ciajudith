da2883be1f1855a1298a4d44d9a98653107fdf50
ciajudith <118665500+ciajudith@users.noreply.github.com>
2023-12-15 18:54:03 +0000

    FizzBuzz Done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,16 @@
 module.exports = function () {
-  // write your code here
+  function fizzbuzz() {
+  const result = [];
+
+  for (let number = 1; number <= 100; number++) {
+      let entry = (number % 3 === 0 ? "Fizz" : "") + (number % 5 === 0 ? "Buzz" : "") || number;
+      result.push(entry);
+  }
+
+  return result;
+}
+
+console.log(fizzbuzz());
+
+
 };
